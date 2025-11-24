3f33545ae9e015f886648a3966e852dde24d0ccb
pack <ngovietbach2402@gmail.com>
2025-11-24 16:59:36 +0700

    css

--- frontend/src/layouts/Sidebar.tsx
+++ frontend/src/layouts/Sidebar.tsx
@@ -3,17 +3,33 @@ import React from "react";
 const Sidebar: React.FC = () => {
   return (
     <aside className="sidebar">
-      <div className="sidebar__service-name">インサイトブリッジ</div>
+      <div className="sidebar-header">インサイトブリッジ</div>
 
-      <nav className="sidebar__nav">
-        <ul className="nav-list">
-          <li><a href="#">ホーム</a></li>
-          <li><a href="#">会話シミュレーション</a></li>
-          <li className="active"><a href="#">メッセージ感情分析</a></li>
-          <li><a href="#">学生理解メモ</a></li>
-          <li><a href="#">コミュニティ掲示板</a></li>
-          <li><a href="#">設定</a></li>
-          <li><a href="#">サインアウト</a></li>
+      <nav className="sidebar-nav">
+        <ul>
+          <li className="nav-item">
+            <a href="#">ホーム</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">会話シミュレーション</a>
+          </li>
+          {/* Mục đang chọn được đánh dấu active */}
+          <li className="nav-item active">
+            <a href="#">メッセージ感情分析</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">学生リスト</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">コミュニティ掲示板</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">設定</a>
+          </li>
+          <li className="nav-separator" />
+          <li className="nav-item signout">
+            <a href="#">サインアウト</a>
+          </li>
         </ul>
       </nav>
     </aside>
